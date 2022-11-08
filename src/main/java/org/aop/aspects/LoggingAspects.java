a542f3cc18f9b42cecafe0f2d9e3d8dce27e769f
pavel.hnelytsia <pavelgnelicya@gmail.com>
2022-11-08 18:25:47 +0200

    // add AoP Advice with some PointCut wildCard PointCut = * = for any return type

--- src/main/java/org/aop/aspects/LoggingAspects.java
+++ src/main/java/org/aop/aspects/LoggingAspects.java
@@ -12,9 +12,10 @@ public class LoggingAspects {
      * It will make some action before Method(signature)
      * <p>
      * PointCut = * = for any method starts with get
+     * PointCut = * = for any return type
      */
     @org.aspectj.lang.annotation.
-            Before("execution(public void get*())")
+            Before("execution(public * get*())")
     public void beforeGetBookAdvice() {
         System.out.println("Some Advice Before Method getBook" +
                 "from any Classes with samy method signature");
